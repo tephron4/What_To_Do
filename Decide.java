4eaff3ae6edeadd4be9948bce7167bdc74f2b5d6
tephron4 <tobiasephron@gmail.com>
2020-02-25 17:49:43 -0500

    Worked on Decide.java

--- Decide.java
+++ Decide.java
@@ -1,3 +1,22 @@
+/**
+ * A class for choosing an option of what to do
+ *
+ * Tobias Ephron
+ * Version 1.0
+ */
+
+import java.util.*;
 class Decide{
 
+
+	/**
+	 * A method from randomly selecting an option for the given User
+	 * 
+	 * @param u the given user for which an option will be chosen
+	 * @return a string of the name of the chosen option
+	 */
+	public String chooseRandom(User u){
+		int ran = Random.nextInt(u.options.size());
+		return u.options.get(ran);
+	}
 }
